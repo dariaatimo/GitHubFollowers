5d2684b4422efad92a568b079ee8e082c0329cf7
vikileon <vileonika@gmail.com>
2025-05-30 21:48:46 +0700

    first commit
    Made app navigation

---
+++ GitHubFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers
+//
+//  Created by Дарья on 30.05.2025.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+
+
+}
