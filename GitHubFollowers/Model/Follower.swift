032218b6f1f9742bbb50bba1a1a9b7829e6ba23a
vikileon <vileonika@gmail.com>
2025-06-13 21:24:10 +0700

    added models for Follower and User

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Дарья on 13.06.2025.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
