e041a9d9b73fc4b1e503087bd3fb70b47ddb8a5d
vikileon <vileonika@gmail.com>
2025-06-17 20:37:12 +0700

    started making Follower CollectionViewCell

---
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,27 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Дарья on 17.06.2025.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
