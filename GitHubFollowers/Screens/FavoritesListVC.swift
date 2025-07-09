204e4535a5b6316154092ac9a39f533de9f27130
vikileon <vileonika@gmail.com>
2025-07-09 21:43:43 +0700

    finished PersistenceManager functionality

--- GitHubFollowers/Screens/FavoritesListVC.swift
+++ GitHubFollowers/Screens/FavoritesListVC.swift
@@ -12,7 +12,14 @@ class FavoritesListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemTeal
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case . failure(let error):
+                break
+            }
+        }
     }
-
-
 }
